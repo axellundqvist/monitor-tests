4c220ee90fe44de6fa83e6aaa323073233b164b4
axellundqvist <axellundkvistrhotmail.com>
2019-07-22 13:13:48 +0200

    Two tests

---
+++ Count/Count/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class Needle
+{
+
+
+    public static int Count(string needle, Stream haystack)
+    {
+        StreamReader reader = new StreamReader(haystack);
+        string text = reader.ReadToEnd();
+
+        return Regex.Matches(text, needle).Count;
+    }
+
+
+
+
+    public static void Main(string[] args)
+    {
+
+        string message = "Hello, there!\nHow are you today?\nYes, you over there.";
+
+        using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(message)))
+
+        Console.WriteLine(Needle.Count("there", stream));
+        Console.ReadLine();
+
+    }
+}
---
+++ Programmer/Programmer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmer
+{
+    public class Programmer
+    {
+        public List<string> Languages = new List<string>();
+
+        public void AddLanguage(string language)
+        {
+            Languages.Add(language);
+        }
+
+    }
+
+    public class ProgrammerTeacher : Programmer
+    {
+        public bool Teach(Programmer programmer, string language)
+        {
+            bool exists = Languages.Any(s => s.Contains(language));
+            if (exists)
+            {
+                programmer.AddLanguage(language);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgrammerTeacher teacher = new ProgrammerTeacher();
+            teacher.AddLanguage("C#");
+
+            Programmer programmer = new Programmer();
+            teacher.Teach(programmer, "C#");
+
+            foreach (var language in programmer.Languages)
+            Console.WriteLine(language);
+
+            Console.ReadLine();
+        }
+    }
+}
